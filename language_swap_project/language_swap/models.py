500ce932d7d30e0dc431b115f64566c877dfe629
Zay Yar Tun <2277073z@student.gla.ac.uk>
2018-03-02 17:25:10 +0000

    second model changes

--- language_swap_project/language_swap/models.py
+++ language_swap_project/language_swap/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class Language(models.Model):
 
-    LanguageName = models.CharField()
+    LanguageName = models.CharField(max_length=120)
 
     def __str__(self):
         return self.LanguageName
@@ -19,28 +19,26 @@ class UserProfile(models.Model):
     #these are the user details that are not in the default User model
     user = models.OneToOneField(User)
 
-    city = models.CharField()
+    city = models.CharField(max_length=120)
 
-    country = models.CharField()
+    country = models.CharField(max_length=120)
 
     picture = models.ImageField(upload_to='profile_images',blank = True)
 
     hobby = models.CharField(max_length=500,blank = True)
 
-    rating = models.ManyToManyField('self',through=Rate,symmetrical=False)
-
     #these are the relationship attributes between user and languages
-    speaks = models.ManyToManyField(Language)
+    speaks = models.ManyToManyField(Language,related_name="speak")
 
-    practices = models.ManyToManyField(Language)
+    practices = models.ManyToManyField(Language,related_name='practice')
 
     #these are the relationship attributes between different users
-    contacts = models.ManyToManyField('self')
+    contacts = models.ManyToManyField('self',related_name='contact')
 
 class Rate(models.Model):
-    ratedUser = models.ForeignKey(UserProfile)
+    ratedUser = models.ForeignKey(UserProfile,related_name='rated')
 
-    rater = models.ForeignKey(UserProfile)
+    rater = models.ForeignKey(UserProfile,related_name='raterUser')
 
     scoreChoices = (
         (1, "bad"),
@@ -50,8 +48,17 @@ class Rate(models.Model):
         (5, "good"),
     )
 
+
     score = models.IntegerField(choices = scoreChoices)
-
-
     def __str__(self):
         return self.user.username
+
+
+
+
+
+
+
+
+
+
