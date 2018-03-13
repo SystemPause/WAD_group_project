914ee8f9be10d1ec847adac6a488e9cbf6042a82
Alessandro Speggiorin <2268690s@student.gla.ac.uk>
2018-03-13 18:23:07 +0000

    Fixed getUserRating(...) helper function

--- language_swap_project/language_swap/helperFunctions.py
+++ language_swap_project/language_swap/helperFunctions.py
@@ -5,14 +5,14 @@ from datetime import date
 from language_swap.models import Contact
 
 # This function retrieves all the informatin regarding one single User.
-# It receives a user (UserProfile) object as parameter and 
+# It receives a user (UserProfile) object as parameter and
 # it returns a dictionary that maps a unique username to a dictionary that
 # contains all the details of the specified user.
 
 def getUserDetails(userObject):
     user_dictionary = {}
-    
-    # Map a username to all the user's details 
+
+    # Map a username to all the user's details
     user_dictionary[userObject.user.username] = {
         'id': userObject.user.id,
         'email' : userObject.user.email,
@@ -28,42 +28,40 @@ def getUserDetails(userObject):
         'gender' : userObject.gender,
         'score' : getUserRating(userObject)
     }
-    
+
     return user_dictionary
 
-# The function computes the age of a specified user. It gets the user date of birth as 
+# The function computes the age of a specified user. It gets the user date of birth as
 # parameter and returns the age
 
 def getUserAge(dateOfBirth):
     todayDate = date.today()
     yearsDifference = todayDate.year - dateOfBirth.year
-    
+
     # Check if the birthday month and day have already passed or not
     monthDayCheck = ((todayDate.month, todayDate.day) < (dateOfBirth.month, dateOfBirth.day))
-    
+
     return  yearsDifference - monthDayCheck
 
 # The function getUserRating(currentobject) computes the rating of a given user
 
 def getUserRating(currentUser):
-    
-    # Get the UserProfile object corresponding to the specified user 
+
+    # Get the UserProfile object corresponding to the specified user
     contactsObject = Contact.objects.filter(contactedUser = currentUser)
 
     if contactsObject.exists():
         # The variable finalScore is used in order to keep track of the user score.
         finalScore = 0.0
-        
+        # Counter is used in order to keep track how many users have rated another user
+        counter = 0
+
+        # Compute the score only for users where the score is not 0 (default value: user not rated yet)
         for contactObject in contactsObject:
-            finalScore +=  contactObject.score
-            
-        return round(finalScore / len(contactsObject),1)
+            if(contactObject.score != 0):
+                finalScore +=  contactObject.score
+                counter += 1
+
+        return round(finalScore / counter,1)
     else:
-        return 0
-    
-    
-        
-        
-        
-    
-    
+        return False
--- language_swap_project/language_swap/views.py
+++ language_swap_project/language_swap/views.py
@@ -9,40 +9,40 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     context_dict = {'languages': []}
-    
+
     # Get all languages from the database
     languages = Language.objects.all().order_by('LanguageName')
-    
+
     # Populate the context_dictionary with all the languages
     for language in languages:
         context_dict['languages'].append(str(language).lower())
-             
+
     return render(request, 'language_swap/index.html', context_dict)
-    
-    
+
+
 def searchResult(request):
     context_dict = {'errors':[], 'users' : {}}
     errors = False
-    
+
     # Get the parameters passed in the URL
     practicingLanguage = request.GET.get('practicingLanguage', '').encode('utf-8').lower().strip()
     spokenLanguage = request.GET.get('spokenLanguage', '').encode('utf-8').lower().strip()
-    
-    # Get a list of places. The list has the format ['city','province','country'] 
+
+    # Get a list of places. The list has the format ['city','province','country']
     places = request.GET.get('places', '').split(",")
 
     # Remove all the empty Unicode values from the list
     places = [place for place in places if place != u'']
-    
+
     # If the list of places is not empty, process the values, otherwise add an error element
     # to the errors list
-    if places: 
+    if places:
         city = places[0].encode('utf-8').lower().strip()
         country = places[-1].encode('utf-8').lower().strip()
     else:
         errors = True
         context_dict['errors'].append("An error has occured while fetching the places.")
-    
+
     # Try to retrieve the selected languages
     try:
         practicing = Language.objects.get(LanguageName = practicingLanguage)
@@ -50,42 +50,42 @@ def searchResult(request):
     except:
         errors = True
         context_dict['errors'].append("An error has occured while fetching the languages.")
-    
-    # If an error occurs while selecting the languages, do not perform the Query and 
+
+    # If an error occurs while selecting the languages, do not perform the Query and
     # just return the error message
     if not errors:
         resultUsers = UserProfile.objects.filter(practices= spoken, speaks = practicing, country = country, city = city)
-        
+
         # If there are results to the Query, retrieve users details
         if resultUsers.exists():
             for resultUser in resultUsers:
-                context_dict['users'].update(getUserDetails(resultUser))      
+                context_dict['users'].update(getUserDetails(resultUser))
         else:
             context_dict['errors'].append("There are no users in this city.")
-            
+
     return render(request, 'language_swap/result.html', context_dict)
 
 def about(request):
     context_dict = {}
-    
+
     return render(request, 'language_swap/about.html', context_dict)
 
 def team(request):
     context_dict = {}
-    
+
     return render(request, 'language_swap/team.html', context_dict)
 
 def contact(request):
     context_dict = {}
-    
+
     return render(request, 'language_swap/contact.html', context_dict)
 
 @login_required
 def profile(request):
     context_dict = {'userProfile' : {}}
-    
+
     loggedUser = UserProfile.objects.get(user = request.user)
-    
+
     # Get all the details of the current logged in user
     context_dict['userProfile'].update(getUserDetails(loggedUser))
 
@@ -94,15 +94,15 @@ def profile(request):
 @login_required
 def contactHistory(request):
     context_dict = {'contacts' : {}}
-    
+
     # Get the current UserProfile object
     loggedUser = UserProfile.objects.get(user = request.user)
-    
+
     # Get the contact list of the users that have been contacted by the logged in user
     contactList = Contact.objects.filter(sourceUser = loggedUser)
-    
+
     for contact in contactList:
         # Populate the context_dict with contacts details
         context_dict['contacts'].update(getUserDetails(contact.contactedUser))
-    
+
     return render(request, 'language_swap/contactHistory.html', context_dict)
