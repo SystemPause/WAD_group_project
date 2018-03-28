4690458a9bdfcb1f2d79d3c6b9788e9071748052
Martina Cocco <2267320c@student.gla.ac.uk>
2018-03-28 15:19:08 +0100

    Quick fixes

--- language_swap_project/language_swap_project/settings.py
+++ language_swap_project/language_swap_project/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = '-$%@&k%0$bx%-*l$mh&(yq=mm5u4*n&afsgku7m(_3-stmk-2o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
